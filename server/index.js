d175f4fa957f686489b3f2ef5459040c8540eaf1
josephkazarian <contact@josephkazarian.com>
2022-01-04 15:58:07 +0100

    added static folder & SPA prepared

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ app.use('/api/users', users);
 const books = require('./routes/api/books');
 app.use('/api/books', books);
 
+// Handle production
+if(process.env.NODE_ENV === 'production') {
+
+    //Static folder
+    app.use(express.static(__dirname + '/public'));
+
+    //Handle Singe Page application
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server Started on port ${port}`));
