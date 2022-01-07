fce75a424a5297321b24e790e946c1c1e6a49740
josephkazarian <contact@josephkazarian.com>
2022-01-07 20:15:31 +0100

    more fixes to image upload

--- server/index.js
+++ server/index.js
@@ -14,36 +14,12 @@ app.use('/api/users', users);
 const books = require('./routes/api/books');
 app.use('/api/books', books);
 
-/*Image Uploading*/
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: function(req, file, callback) {
-    callback(null, '/src/my-images');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname);
-  }
-});
-
-// import morgan from 'morgan';
-// import express from 'express';
-// const multer = require('multer');
-// const upload = multer({dest: 'uploads/'});
-
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(morgan('dev'));
-
-// app.use(express.static(__dirname, 'public'));
-/* End image uploading*/
-
 // Handle production
 if(process.env.NODE_ENV === 'production') {
 
     //Static folder
     app.use(express.static(__dirname + '/public'));
-
+    app.get('/userimages/:image', (req, res) => res.sendFile(__dirname + '/userImages/' + req.params.image));
     //Handle Singe Page application
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
